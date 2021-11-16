916f571a4d45a3233921813bcfce9323d67e45ec
Yoszimitsu <cotam07@gmail.com>
2021-11-17 00:07:38 +0100

    added check_level function

--- modules/SampleModule/main.py
+++ modules/SampleModule/main.py
@@ -6,9 +6,12 @@ import random
 import time
 import sys
 import iothub_client
+import json
+import asyncio
 # pylint: disable=E0611
 from iothub_client import IoTHubModuleClient, IoTHubClientError, IoTHubTransportProvider
 from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError
+from pyModbusTCP.client import ModbusClient
 
 # messageTimeout - the maximum time in milliseconds until a message times out.
 # The timeout period starts at IoTHubModuleClient.send_event_async.
@@ -18,6 +21,7 @@ MESSAGE_TIMEOUT = 10000
 # global counters
 RECEIVE_CALLBACKS = 0
 SEND_CALLBACKS = 0
+THRESHOLD = 70.0
 
 # Choose HTTP, AMQP or MQTT as transport protocol.  Currently only MQTT is supported.
 PROTOCOL = IoTHubTransportProvider.MQTT
@@ -49,6 +53,62 @@ def receive_message_callback(message, hubManager):
     hubManager.forward_event_to_output("output1", message, 0)
     return IoTHubMessageDispositionResult.ACCEPTED
 
+async def check_level(hubManager):
+    # Customize this coroutine to do whatever tasks the module initiates
+    # TCP auto connect on first modbus request
+    iologik = ModbusClient(host="192.168.4.254", port=502, unit_id=1, auto_open=True)
+    iologik.open()
+    alert = False
+    message = ""
+    while True:    
+        # request will get a list --> cast first element to int or float
+        level = iologik.read_input_registers(513,1)
+        # check if read successful
+        if level:
+            print("")
+        else:
+            level = [-1]
+            print("Level read error")
+
+        #create float
+        level = float(level[0])
+
+        # Calculate Point Slope as in ThingsPro Gateway
+        # Point-slope: OUTPUT = ((INPUT-sourceMin) * (targetMax-targetMin) / (sourceMax-sourceMin)) + targetMin
+        # point_slope(INPUT, sourceMin, sourceMax, targetMin, targetMax):
+        level = point_slope(level, 0, 65535, 0, 100)
+        print("Level %lf" % level)
+        # LED Logic from depending on level2
+        if level >= THRESHOLD:
+            alert = True
+            iologik.write_single_coil(0,1)
+            print("Level -> %1f >= %1f, ALERT = %r", level, THRESHOLD, alert)
+            message = create_message({"Level: %1f", level}, "ALERT")
+        elif alert == True:
+            iologik.write_single_coil(0,0)
+            print("Level -> %1f < %1f, ALERT = %r", level, THRESHOLD, alert)
+            alert = False
+            message = create_message({"Level: %1f", level}, "ALERT")
+        else:
+            message = ""
+
+        # sending message to IoTHub
+        if message != "":
+            await hubManager.forward_event_to_output("output1", message, 0)
+
+        await asyncio.sleep(1)
+    iologik.close()
+
+def create_message(input, type):
+        message_json = json.loads(input)
+        message_json.custom_properties["MessageType"] = type
+        return message_json
+
+def point_slope(INPUT, sourceMin, sourceMax, targetMin, targetMax):
+    # Calculate Point Slope as in ThingsPro Gateway
+    # Point-slope: OUTPUT = ((INPUT-sourceMin) * (targetMax-targetMin) / (sourceMax-sourceMin)) + targetMin
+    OUTPUT = ((INPUT-sourceMin) * (targetMax-targetMin) / (sourceMax-sourceMin)) + targetMin
+    return OUTPUT
 
 class HubManager(object):
 
@@ -79,10 +139,8 @@ def main(protocol):
         hub_manager = HubManager(protocol)
 
         print ( "Starting the IoT Hub Python sample using protocol %s..." % hub_manager.client_protocol )
-        print ( "The sample is now waiting for messages and will indefinitely.  Press Ctrl-C to exit. ")
-
-        while True:
-            time.sleep(1)
+        check_level(hub_manager)
+        print ( "The sample is now checking level and will indefinitely.  Press Ctrl-C to exit. ")
 
     except IoTHubError as iothub_error:
         print ( "Unexpected error %s from IoTHub" % iothub_error )
